8513e8b66f5a2c6b69667a043b0a7d8b144fdeba
wkwk <420220193@qq.com>
2018-12-12 14:52:00 +0800

    ss

--- app/src/main/java/com/example/wk/processview/MainActivity.kt
+++ app/src/main/java/com/example/wk/processview/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.wk.processview
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wk.processview.R.id.round
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        round.setProcess(BigDecimal.TEN, BigDecimal.TEN,Color.GREEN)
     }
 }
